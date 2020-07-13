b128f1824342a41740d1eecc6a3c891cc8636cf9
Akarsh <akarshgupta2000@gmail.com>
2020-07-13 18:00:05 +0530

    fifth commit

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
-const express = require("express");
-const bodyParser = require("body-parser");
-const request = require("request");
-const https = require("https");
+//jshint esversion: 6
+const express = require("express");//requires installation
+const bodyParser = require("body-parser");//requires installation
+const request = require("request");//requires installation
+const https = require("https");//used to get or post data from anc external resource/url.
 const app = express();
 app.use(express.static("public"));//for letting the server to access our static files
 //that is the local files we need to use app.use(express.static("foldername/filename"));
@@ -26,7 +27,7 @@ app.post("/", function(req, res){
       }
     ]
   };
-  const jsonData = JSON.stringify(data);
+  const jsonData = JSON.stringify(data);//changes the format of data from json to a sting.
   const url = "https://us10.api.mailchimp.com/3.0/lists/8d1b35726d";
   const options = {
     method: "POST",
@@ -39,7 +40,7 @@ app.post("/", function(req, res){
       res.sendFile(__dirname+"/failure.html");
     }
     response.on("data", function(data){
-      console.log(JSON.parse(data));
+      console.log(JSON.parse(data));//will give a specified entry in the hyper terminal
     })
   })
   request.write(jsonData);
@@ -47,7 +48,7 @@ app.post("/", function(req, res){
 });
 
 app.post("/failure", function(req, res){
-  res.redirect("/");
+  res.redirect("/");//if we clickupon the button on the failure page then it will take us back to the signup page.
 })
 
 
@@ -61,5 +62,8 @@ app.listen(process.env.PORT || 3000, function(){//since we are using the
 
   console.log("the server is currently running at port 3000");
 });
+//Use a Procfile, a text file in the root directory of your application, to explicitly declare what command should be executed to start your app
+//web: node index.js should be inide that procfile
+//This declares a single process type, web, and the command needed to run it. The name web is important here. It declares that this process type will be attached to the HTTP routing stack of Heroku, and receive web traffic when deployed.
 //cd5440f1feffc45cdd7a5f0627b9b05d-us10
 //audience-id/list-id: 8d1b35726d
